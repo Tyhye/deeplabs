067340e28e8150942d63ea40d4937a540c1bfb04
tyhye <wzy0304@outlook.com>
2018-08-23 11:45:09 +0800

    global ignore

---
+++ experiment/process/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+# -*- code:utf-8 -*- 
+'''
+ @Author: tyhye.wang 
+ @Date: 2018-08-12 23:39:22 
+ @Last Modified by:   tyhye.wang 
+ @Last Modified time: 2018-08-12 23:39:22 
+'''
+
+"""
+Epoch Processor and Iter Processor are designed in this package
+"""
